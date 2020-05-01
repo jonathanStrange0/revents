0f1497da07b1325a8ea03c6680702dbdd2e742c6
Jonathan Mucha <jonmucha@gmail.com>
2020-05-01 05:48:35 -0400

    added bunches of components ahead of adding reach router

---
+++ src/features/user/PeopleDashboard/PeopleDashnboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const PeopleDashnboard = () => {
+    return (
+        <div>
+            <h1>People Dashboard</h1>
+        </div>
+    )
+}
+export default PeopleDashboard
